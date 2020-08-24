c90f7aa06c5e08172dfc1c7435f0d360d165b766
TriNguyenGit <nguyenvantri51480153@gmail.com>
2020-08-24 19:27:45 +0700

    attrs commit

---
+++ app/src/test/java/TextViewCircle.java
@@ -0,0 +1,24 @@
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.example.customview13072020.MainActivity;
+
+public class TextViewCircle extends LinearLayout {
+
+    public TextViewCircle(Context context) {
+        super(context);
+    }
+
+    public TextViewCircle(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextViewCircle(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
